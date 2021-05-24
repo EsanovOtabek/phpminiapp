c1605843e1fae2f11121fc8b5d83ddd2323b6740
Esanov Otabek <70377304+EsanovOtabek@users.noreply.github.com>
2021-05-24 09:52:55 +0500

    Sahifani ko'rinishi yangilandi

--- view.php
+++ view.php
@@ -18,20 +18,30 @@ $row=mysqli_fetch_assoc($res);
 
 
 <? include_once 'pages/head.php'; ?>
-<body class="p-5">
-
-<div class="container-fluid">
-	<div class="row">
+<body><hr>
+<div class="container">
+	<div class="m-5"></div>
+	<div class="row border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative m-1 p-1 pb-5 pt-4">
 		<div class="col-lg-9"><h2><?=$row['name']?></h2></div>
 		<div class="col-lg-3 text-center"><a href="index.php" class=" text-white btn btn-primary">Go Home</a></div>
 		<div class="col-md-12">
-			<div class="card bg-dark p-3 text-white">
-				<h3><?=$row['title']?></h3>
+			<div class="card bg-dark p-3 text-white mt-2">
+				<h3 class="text-center"><?=$row['title']?></h3>
 				<p class="">
-					<span><i class="fa fa-eye"></i> <?=$row['mdate']?></span><br>
-					<span><i class="fa fa-file"></i> <?=$row['file_name']?></span><br>
+					<div class="row p-2">
+						<div class="col-lg-4">
+							<span class="btn btn-primary text-light w-100"><i class="fa fa-clock"></i> O'tkazilgan sanasi: <?=$row['mdate']?></span>
+						</div>
+						<div class="col-lg-4"><br></div>
+						<div class="col-lg-4">
+							<span><a class="btn btn-primary text-light w-100" href="files/<?=$row['file_name']?>"> <i class="fa fa-download"></i> Faylni yuklab olish.</a></span>
+						</div>
+					</div>
+					
+					
 				</p >
-				<p class=""><?=$row['content']?></p>
+				<p class="p-2"><?=$row['content']?></p>
+				<div class="card-footer text-light row p-0 pt-4"><div class="col-lg-9"> </div><div class="col-lg-3"><p> Qo'shilgan sana: <?=$row['addtime']?></p></div> </div>
 			</div>
 		</div>	
 
